131f5b726b6d412a783b0503f5900ec35fb25931
Johan Klausen <klausenjohan23@gmail.com>
2023-09-06 10:18:44 +0200

    Add displayTasks function

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 
 var tasks = ["Buy milk", "Clean room", "Pay bills"];
+
+function displayTasks() {
+  var taskList = document.getElementById("taskList");
+  taskList.innerHTML = "";
+  for (let i = 0; i < tasks.length; i++) {
+    taskList.innerHTML += "<li>" + task[i] + "</li>";
+  }
+}
